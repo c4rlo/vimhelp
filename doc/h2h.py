6ec61972cda43d7ffb2512e4f80a2875bd5279da
Carlo Teubner <carlo.teubner@gmail.com>
2013-04-27 12:41:09 +0100

    make commandline h2h.py a bit more usable

--- doc/h2h.py
+++ doc/h2h.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python
 
-import sys
-import os.path
-import cProfile
+import sys, os, os.path
+#import cProfile
 
 sys.path.append('../gae')
 
@@ -15,27 +14,39 @@ def slurp(filename):
     return c
 
 def usage():
-    return "usage: " + sys.argv[0] + " <file>..."
+    return "usage: " + sys.argv[0] + " IN_DIR OUT_DIR [BASENAMES...]"
 
 def main():
-    if len(sys.argv) < 2: sys.exit(usage())
+    if len(sys.argv) < 3: sys.exit(usage())
+
+    in_dir = sys.argv[1]
+    out_dir = sys.argv[2]
+    basenames = sys.argv[3:]
 
     print "Processing tags..."
-    h2h = VimH2H(slurp('tags'))
+    h2h = VimH2H(slurp(os.path.join(in_dir, 'tags')))
 
-    for filename in sys.argv[1:]:
-        print "Processing " + filename + "..."
-        text = slurp(filename)
+    if len(basenames) == 0:
+        basenames = os.listdir(in_dir)
+
+    for basename in basenames:
+        if os.path.splitext(basename)[1] != '.txt' and basename != 'tags':
+            print "Ignoring " + basename
+            continue
+        print "Processing " + basename + "..."
+        path = os.path.join(in_dir, basename)
+        text = slurp(path)
         try:
             text.decode('UTF-8')
         except UnicodeError:
             encoding = 'ISO-8859-1'
         else:
             encoding = 'UTF-8'
-        of = open(filename + '.html', 'w')
-        of.write(h2h.to_html(filename, slurp(filename), encoding,
+        outpath = os.path.join(out_dir, basename + '.html')
+        of = open(outpath, 'w')
+        of.write(h2h.to_html(basename, slurp(path), encoding,
                              web_version=False))
         of.close()
 
-#main()
-cProfile.run('main()')
+main()
+#cProfile.run('main()')
