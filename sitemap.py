770a8215e9d446f662418aa053dc24d860246f10
Carlo Teubner <carlo.teubner@gmail.com>
2015-10-25 11:34:18 +0000

    generate sitemap.txt programmatically

---
+++ sitemap.py
@@ -0,0 +1,22 @@
+# Generate 'sitemap.txt' on the fly.
+
+import dbmodel
+import operator
+import webapp2
+
+BASE_URL = 'http://vimhelp.appspot.com/'
+
+class PageHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.content_type = 'text/plain'
+        query = dbmodel.ProcessedFileHead.query()
+        all_names = \
+                query.map(operator.methodcaller('string_id'), keys_only=True)
+        self.response.write(BASE_URL + '\n')
+        for name in sorted(all_names):
+            self.response.write(BASE_URL + name + '.html\n')
+
+
+app = webapp2.WSGIApplication([
+    (r'/sitemap\.txt', PageHandler)
+])
