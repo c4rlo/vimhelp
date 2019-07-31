c20ce2c02935c72c0cd46044feb8dea0c6357982
Carlo Teubner <carlo@cteubner.net>
2019-07-31 07:23:26 +0100

    In-process caching

--- vimhelp.py
+++ vimhelp.py
@@ -2,6 +2,7 @@
 
 import datetime
 import logging
+import threading
 import webapp2
 from webob.exc import HTTPNotFound, HTTPInternalServerError
 from google.appengine.ext import ndb
@@ -12,13 +13,15 @@ HTTP_NOT_MOD = 304
 LEGACY_HOSTS = 'www.vimhelp.org', 'vimhelp.appspot.com'
 LEGACY_HOST_URLS = ('http://vimhelp.org',)
 
+g_cache = {}
+g_cache_lock = threading.Lock()
+
 
 class PageHandler(webapp2.RequestHandler):
     def get(self, filename):
         req = self.request
         resp = self.response
-        logging.debug("url = '{}', host_url = '{}', path_qs = '{}'".format(
-            req.url, req.host_url, req.path_qs))
+
         if req.host in LEGACY_HOSTS or req.host_url in LEGACY_HOST_URLS:
             new_url = 'https://vimhelp.org' + req.path_qs
             logging.info("Redirecting to: {}".format(new_url))
@@ -29,38 +32,73 @@ class PageHandler(webapp2.RequestHandler):
             filename = 'help.txt'
         elif not filename.endswith('.txt') and filename != 'tags':
             return self.redirect('/' + filename + '.txt.html', permanent=True)
-        head = ProcessedFileHead.get_by_id(filename)
-        if not head:
-            logging.warn("%s not found in db", filename)
-            raise HTTPNotFound()
-        resp.etag = head.etag
-        # set expires to next exact half hour, i.e. :30:00 or :00:00
-        # TODO: race condition if it's now :30:01 and new version is in the
-        # process of being generated
-        expires = datetime.datetime.utcnow().replace(second=0, microsecond=0)
-        expires += datetime.timedelta(minutes=(30 - (expires.minute % 30)))
-        resp.expires = expires  # TODO this doesn't seem to work; need to use
-                                # 'cache_expires'
-        resp.last_modified = head.modified
-        del resp.cache_control
-        if head.etag in req.if_none_match:
-            logging.info("matched etag, modified %s, expires %s",
-                         resp.last_modified, expires)
-            resp.status = HTTP_NOT_MOD
-        elif not req.if_none_match and req.if_modified_since and \
-                req.if_modified_since >= resp.last_modified:
-            logging.info("not modified since %s, modified %s, expires %s",
-                         req.if_modified_since, resp.last_modified, expires)
-            resp.status = HTTP_NOT_MOD
-        else:
-            parts = get_parts(head)
-            logging.info("writing %d-part response, modified %s, expires %s",
-                         1 + len(parts), resp.last_modified, expires)
-            resp.content_type = 'text/html'
-            resp.charset = head.encoding
-            resp.write(head.data0)
-            for part in parts:
-                resp.write(part.data)
+
+        now = datetime.datetime.utcnow()
+
+        with g_cache_lock:
+            entry = g_cache.get(filename)
+
+        if entry is not None:
+            head, parts, timestamp = entry
+            if next_update_time(timestamp) > now:
+                logging.debug("responding from inproc cache entry")
+                prepare_response(req, resp, head, now)
+                if resp.status != HTTP_NOT_MOD:
+                    write_response(resp, head, parts)
+            else:
+                logging.debug("inproc cache entry is expired")
+                entry = None
+
+        if entry is None:
+            head = ProcessedFileHead.get_by_id(filename)
+            if not head:
+                logging.warn("%s not found in db", filename)
+                raise HTTPNotFound()
+            logging.debug("responding from db")
+            prepare_response(req, resp, head, now)
+            parts = []
+            if resp.status != HTTP_NOT_MOD:
+                parts = get_parts(head)
+                write_response(resp, head, parts)
+            if head.numparts == 1 or parts:
+                logging.debug("writing entry to inproc cache")
+                with g_cache_lock:
+                    g_cache[filename] = head, parts, now
+
+
+def prepare_response(req, resp, head, now):
+    resp.etag = head.etag
+    expires = next_update_time(now)
+    resp.expires = expires  # TODO this doesn't seem to work; need to use
+                            # 'cache_expires'
+    resp.last_modified = head.modified
+    del resp.cache_control
+    if head.etag in req.if_none_match:
+        logging.info("matched etag, modified %s, expires %s",
+                     resp.last_modified, expires)
+        resp.status = HTTP_NOT_MOD
+    elif not req.if_none_match and req.if_modified_since and \
+            req.if_modified_since >= resp.last_modified:
+        logging.info("not modified since %s, modified %s, expires %s",
+                     req.if_modified_since, resp.last_modified, expires)
+        resp.status = HTTP_NOT_MOD
+
+
+# Return next exact half hour, i.e. HH:30:00 or HH:00:00
+def next_update_time(t):
+    r = t.replace(second=0, microsecond=0)
+    r += datetime.timedelta(minutes=(30 - (t.minute % 30)))
+    return r
+
+
+def write_response(resp, head, parts):
+    logging.info("writing %d-part response, modified %s, expires %s",
+                 1 + len(parts), resp.last_modified, resp.expires)
+    resp.content_type = 'text/html'
+    resp.charset = head.encoding
+    resp.write(head.data0)
+    for part in parts:
+        resp.write(part.data)
 
 
 def get_parts(head):
