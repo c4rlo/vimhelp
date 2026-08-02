a04e92cf9de274e68563d7705db300602ca091b5
Carlo Teubner <carlo@cteubner.net>
2026-08-02 11:44:26 +0100

    Add Cache-Control headers for {robots,sitemap}.txt

--- vimhelp/robots.py
+++ vimhelp/robots.py
@@ -7,15 +7,17 @@ import flask
 from . import dbmodel
 
 BASE_URLS = {
-    "vim": "https://vimhelp.org/",
-    "neovim": "https://neo.vimhelp.org/",
+    "vim": "https://vimhelp.org",
+    "neovim": "https://neo.vimhelp.org",
 }
 
 
 def handle_robots_txt():
-    return flask.Response(
+    resp = flask.Response(
         f"Sitemap: {BASE_URLS[flask.g.project]}/sitemap.txt\n", mimetype="text/plain"
     )
+    resp.cache_control.max_age = 3600 * 24
+    return resp
 
 
 def handle_sitemap_txt():
@@ -29,12 +31,14 @@ def handle_sitemap_txt():
         names = set(query.map(lambda key: key.id().split(":")[-1], keys_only=True))
         names.discard("help.txt")
 
-    return flask.Response(
+    resp = flask.Response(
         "".join(
             itertools.chain(
-                (f"{base_url}\n",),
-                (f"{base_url}{name}.html\n" for name in sorted(names)),
+                (f"{base_url}/\n",),
+                (f"{base_url}/{name}.html\n" for name in sorted(names)),
             )
         ),
         mimetype="text/plain",
     )
+    resp.cache_control.max_age = 15 * 60
+    return resp
